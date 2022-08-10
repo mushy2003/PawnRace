2ae5a28f7ff96089583b8ed2fb002702115c3b26
mh1021 <musawer.hussain21@imperial.ac.uk>
2022-08-10 17:51:25 +0100

    ai works.. needs some improvement though

--- src/main/Java/Player.java
+++ src/main/Java/Player.java
@@ -27,17 +27,34 @@ public class Player {
     return game.allValidMoves(piece);
   }
 
+  public int numberOfPassedPawns() {
+    List<Position> positions = board.positionsOf(piece);
+    int count = 0;
+
+    for (Position position : positions) {
+      if (isPassedPawn(position)) {
+        count++;
+      }
+    }
+
+    return count;
+  }
+
   public boolean isPassedPawn(Position position) {
     if (board.pieceAt(position).isEmpty()) {
       return false;
     }
     List<Position> opponentPawns = opponent.getAllPawns();
     for (Position oppPosition : opponentPawns) {
-      if (piece == Piece.WHITE && position.getRankIndex() <= oppPosition.getRankIndex()) {
+      if (piece == Piece.WHITE
+          && position.getRankIndex() < oppPosition.getRankIndex()
+          && Math.abs(position.getFileIndex() - oppPosition.getFileIndex()) <= 1) {
         return false;
       }
 
-      if (piece == Piece.BLACK && position.getRankIndex() >= oppPosition.getRankIndex()) {
+      if (piece == Piece.BLACK
+          && position.getRankIndex() > oppPosition.getRankIndex()
+          && Math.abs(position.getFileIndex() - oppPosition.getFileIndex()) <= 1) {
         return false;
       }
     }
@@ -45,13 +62,115 @@ public class Player {
     return true;
   }
 
+//  public Move makeMove(Game game) {
+//    List<Move> validMoves = getAllValidMoves();
+//    Random generator = new Random();
+//    Move randomMove = validMoves.get(generator.nextInt(validMoves.size() - 1));
+//    System.out.println("Ai played the move: " + randomMove);
+//    game.applyMove(randomMove);
+//    return randomMove;
+//  }
+
   public Move makeMove(Game game) {
-    List<Move> validMoves = getAllValidMoves();
-    Random generator = new Random();
-    Move randomMove = validMoves.get(generator.nextInt(validMoves.size() - 1));
-    System.out.println("Ai played the move: " + randomMove);
-    game.applyMove(randomMove);
-    return randomMove;
+    List<Move> moves = getAllValidMoves();
+    int maxScore = Integer.MIN_VALUE;
+    Move bestMove = null;
+    Player currentPlayer = (piece == Piece.WHITE) ? this : opponent;
+
+    for (Move move : moves) {
+      game.applyMove(move);
+      int currentScore = minimax(8, Integer.MIN_VALUE, Integer.MAX_VALUE, currentPlayer.getOpponent());
+      if (currentScore > maxScore) {
+        maxScore = currentScore;
+        bestMove = move;
+      }
+      game.unApplyMove();
+    }
+
+    game.applyMove(bestMove);
+
+    return bestMove;
+  }
+
+  private int minimax(int depth, int alpha, int beta, Player currentPlayer) {
+    if (currentPlayer.game.gameOver() && game.winner() == null) {
+      return 0;
+    }
+
+    if (currentPlayer.game.gameOver() && game.winner().piece == Piece.WHITE) {
+      return 100000;
+    }
+
+    if (currentPlayer.game.gameOver() && game.winner().piece == Piece.BLACK) {
+      return -100000;
+    }
+
+    if (depth == 0) {
+      if (currentPlayer.piece == Piece.WHITE) {
+        return evaluateScore(currentPlayer);
+      }
+
+      return evaluateScore(currentPlayer) * -1;
+      //return board.positionsOf(Piece.WHITE).size() - board.positionsOf(Piece.BLACK).size();
+    }
+
+    if (currentPlayer.piece == Piece.WHITE) {
+      List<Move> moves = currentPlayer.getAllValidMoves();
+      int maxScore = Integer.MIN_VALUE;
+
+      for (Move move : moves) {
+        currentPlayer.game.applyMove(move);
+        int currentScore = minimax(depth - 1, alpha, beta, currentPlayer.getOpponent());
+        currentPlayer.game.unApplyMove();
+        maxScore = Math.max(maxScore, currentScore);
+        alpha = Math.max(currentScore, alpha);
+        if (beta <= alpha) {
+          break;
+        }
+      }
+
+      return maxScore;
+    }
+
+    List<Move> moves = currentPlayer.getAllValidMoves();
+    int minScore = Integer.MAX_VALUE;
+
+    for (Move move : moves) {
+      currentPlayer.game.applyMove(move);
+      int currentScore = minimax(depth - 1, alpha, beta, currentPlayer.getOpponent());
+      currentPlayer.game.unApplyMove();
+      minScore = Math.min(minScore, currentScore);
+      beta = Math.min(currentScore, beta);
+      if (beta <= alpha) {
+        break;
+      }
+    }
+
+    return minScore;
+  }
+
+  private int evaluateScore(Player currentPlayer) {
+    Board board = currentPlayer.board;
+    Piece piece = currentPlayer.piece;
+    Player opponent = currentPlayer.opponent;
+    List<Position> positions = board.positionsOf(piece);
+
+    int currentScore = board.positionsOf(piece).size() - board.positionsOf(piece.getOpposite()).size();
+    currentScore += (currentPlayer.numberOfPassedPawns() - opponent.numberOfPassedPawns()) * 10;
+
+    for (Position position : positions) {
+      if (piece == Piece.WHITE && position.getRankIndex() >= 5) {
+        currentScore += 100;
+      }
+
+
+
+      if (piece == Piece.BLACK && position.getRankIndex() <= 2) {
+        currentScore += 100;
+      }
+    }
+
+    return currentScore;
   }
 
   public Player getOpponent() {
